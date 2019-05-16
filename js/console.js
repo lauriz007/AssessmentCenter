88892f427b68164515a01631127c455af2f169c6
pauliustumas <45540620+pauliustumas@users.noreply.github.com>
2019-05-16 10:24:43 +0300

    Add files via upload

--- js/console.js
+++ js/console.js
@@ -86,5 +86,28 @@ function loadTask(sampleId) {
 	
 }
 
+function saveSolution(){
+	var el = document.getElementById('jscontent');
+	download(el.value, "solution_" + sampleId, ".txt");
+}
+
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
 
 
