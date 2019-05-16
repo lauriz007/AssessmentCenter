57ce46c6ec5fdf456a29d025851a546aa88d1618
pauliustumas <45540620+pauliustumas@users.noreply.github.com>
2019-05-16 09:28:04 +0300

    Test example

---
+++ js/console.js
@@ -0,0 +1,90 @@
+var sampleId = 0;
+
+var tasks = [
+
+'// TASK #1.\n'+
+'//Finish convert function to parse and print year, month, day, hour, minute, seconds and\n'+
+'//millis from provided data.\n\n'+
+
+'//Input: date in a string.  Like: 2018-08-13 17.00.05:542;\n'+
+'//Expected output from a function in console:\n'+
+'//Current time is 17 hours, 00 min, 05 sec & 542 millis. Date: 13/08/2017.\n\n'+
+
+'function convert(input){\n'+
+'	var output = "Current time is 17 hours, 00 min, 05 sec & 542 millis. Date: 13/08/2017.";\n\n'+	
+'       //your solution should be here\n\n'+
+'	return output;\n'+
+'}\n\n'+
+
+"console.log(convert('2018-08-13 17.00.05:542'));\n"
+
+
+
+];
+
+var tests = [
+
+"var testSample = '2018-08-13 17.00.05:542';\n"+
+'var output = "Current time is 17 hours, 00 min, 05 sec & 542 millis. Date: 13/08/2017."\n'+
+
+'if(convert(testSample) == output){\n'+
+'	alert("Solution is good");\n'+
+'}\n'+
+'else{\n'+
+'	alert("Solution is bad");\n'+
+'}\n'
+
+
+]
+
+function testSolution(){
+	
+	var el = document.getElementById('jscontent');
+   	
+    var oldScript = document.getElementById('scriptContainer');
+		
+    var newScript;
+
+    if (oldScript) {
+      oldScript.parentNode.removeChild(oldScript);
+    }
+	
+	var userSript = el.value;
+	    userSript = userSript + tests[sampleId];
+		
+    newScript = document.createElement('script');
+    newScript.id = 'scriptContainer';
+    newScript.text = userSript;
+    document.body.appendChild(newScript);
+}
+
+
+
+var former = console.log;
+	console.log = function (msg) {
+    former(msg);
+
+    var ul = document.getElementById("log");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(msg));
+    ul.appendChild(li)
+
+}
+
+function clearLog() {
+    document.getElementById("log").innerHTML = "";
+    Materialize.toast("Console Cleared", 4000)
+}
+
+function reloadPage() {
+    location.reload();
+}
+
+function loadTask(sampleId) {
+	this.sampleId = sampleId; 
+    document.getElementById('jscontent').value = tasks[sampleId];
+	
+}
+
+
+
